3abb9817eba46bd51258953e90196d3adb0713df
kevinceausu <74996864+kevinceausu@users.noreply.github.com>
2020-11-29 21:57:24 +0000

    Alphaproject added

---
+++ Kevin Ceausu - Personal Project/Assets/Scripts2/LevelControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelControl : MonoBehaviour
+
+{
+    [SerializeField] private string newLevel;
+    [SerializeField] private GameObject uiElement;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            uiElement.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                SceneManager.LoadScene(newLevel);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            uiElement.SetActive(false);
+        }
+    }
+
+
+    public string levelName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+
+    }
+
+
+
+
